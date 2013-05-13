7d2d1b20c4a3d75610acc0782795cb8af8411a40
watershao <watershao123@gmail.com>
2013-05-13 18:17:18 +0800

    20130513

---
+++ src/com/android/flypigeon/util/Person.java
@@ -0,0 +1,28 @@
+package com.android.flypigeon.util;
+
+import java.io.Serializable;
+
+/**
+ * 每个用户的相关信息
+ * @author v_watershao
+ *
+ */
+public class Person implements Serializable{
+	private static final long serialVersionUID = 1L;
+	public int personId = 0;
+	public int personHeadIconId = 0;
+	public String personNickeName = null;
+	public String ipAddress = null;
+	public String loginTime = null;
+	public long timeStamp = 0;
+	public int groupId = 0;
+	
+	public Person(int personId,int personHeadIconId,String personNickeName,String ipAddress,String loginTime){
+		this.personId = personId;
+		this.personHeadIconId = personHeadIconId;
+		this.personNickeName = personNickeName;
+		this.ipAddress = ipAddress;
+		this.loginTime = loginTime;
+	}
+	public Person(){}
+}
